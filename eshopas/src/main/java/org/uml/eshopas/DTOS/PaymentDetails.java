d6879e6afe88fd0d1b3acfd46db0b79456cfbb27
Marius <mariukas.ambrazevicius@gmail.com>
2025-04-12 19:26:25 +0300

    Minor fix.

--- eshopas/src/main/java/org/uml/eshopas/DTOS/PaymentDetails.java
+++ eshopas/src/main/java/org/uml/eshopas/DTOS/PaymentDetails.java
@@ -5,18 +5,18 @@ import jakarta.validation.constraints.NotEmpty;
 
 public record PaymentDetails(
 
-        @NotEmpty(message = "Name must not be empty")
+        @NotEmpty(message = "Name must not be empty.")
         String name,
 
         @NotEmpty(message = "Email must not be empty")
         @Email(regexp = "^(?=.{1,64}@)[\\p{L}0-9_-]+(\\.[\\p{L}0-9_-]+)*@[^-][\\p{L}0-9-]+(\\.[\\p{L}0-9-]+)*(\\.[\\p{L}]{2,})$",
-                message = "email must in valid format")
+                message = "Email must in valid format.")
         String email,
 
-        @NotEmpty(message = "phone must not be empty.")
+        @NotEmpty(message = "Phone must not be empty.")
         String phone,
 
-        @NotEmpty(message = "address must not be empty.")
+        @NotEmpty(message = "Address must not be empty.")
         String address,
 
         String paymentIntentId
